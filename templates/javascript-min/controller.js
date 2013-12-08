8547c5523cdb5a1596313c12c81b749754bbc314
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-12-08 17:18:16 +0100

    Edited main controller and view to use the PHP backend

--- templates/javascript-min/controller.js
+++ templates/javascript-min/controller.js
@@ -2,9 +2,36 @@
 
 angular.module('<%= scriptAppName %>')
   .controller('<%= classedName %>Ctrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.awesomeThings = [];
+    $scope.loading = true;
+
+    // Get awesome things list
+    $http({method: 'GET', url: '/api/resource.php'}).
+
+      success(function (data) {
+        $scope.loading = false;
+        $scope.awesomeThings = data.resources;
+
+        // Get description of each thing
+        $scope.awesomeThings.forEach(function (thing) {
+          thing.loading = true;
+
+          $http({method: 'GET', url: thing.href}).
+            success(function (data) {
+              thing.loading = false;
+              thing.description = data.description;
+            }).
+            error(function (data) {
+              thing.loading = false;
+              thing.error = data.description || 'Server is unreachable';
+            });
+        });
+      }).
+
+      error(function (data) {
+        $scope.loading = false;
+        $scope.error = data.description || 'Server is unreachable';
+      });
+
   }]);
--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -1,10 +1,37 @@
 'use strict';
 
 angular.module('<%= scriptAppName %>')
-  .controller('<%= classedName %>Ctrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('<%= classedName %>Ctrl', function ($scope, $http) {
+
+    $scope.awesomeThings = [];
+    $scope.loading = true;
+
+    // Get awesome things list
+    $http({method: 'GET', url: '/api/resource.php'}).
+
+      success(function (data) {
+        $scope.loading = false;
+        $scope.awesomeThings = data.resources;
+
+        // Get description of each thing
+        $scope.awesomeThings.forEach(function (thing) {
+          thing.loading = true;
+
+          $http({method: 'GET', url: thing.href}).
+            success(function (data) {
+              thing.loading = false;
+              thing.description = data.description;
+            }).
+            error(function (data) {
+              thing.loading = false;
+              thing.error = data.description || 'Server is unreachable';
+            });
+        });
+      }).
+
+      error(function (data) {
+        $scope.loading = false;
+        $scope.error = data.description || 'Server is unreachable';
+      });
+
   });
