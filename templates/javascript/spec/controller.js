59ceb493b8fe555029e75aa30ab0f6080068ffc3
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-01-20 19:37:17 +0100

    Fixed unit tests, 'grunt test' now passing

--- templates/javascript/spec/controller.js
+++ templates/javascript/spec/controller.js
@@ -16,7 +16,50 @@ describe('Controller: <%= classedName %>Ctrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should attach a list of awesomeThings to the scope', inject(function ($httpBackend) {
+
+    $httpBackend.whenGET('/api/resource.php').respond({
+      resources: [
+        {
+          id: 'html5-boilerplate',
+          name: 'HTML5 Boilerplate',
+          href: '/api/resource.php?id=html5-boilerplate'
+        },
+        {
+          id: 'angular',
+          name: 'Angular',
+          href: '/api/resource.php?id=angular'
+        },
+        {
+          id: 'karma',
+          name: 'Karma',
+          href: '/api/resource.php?id=karma'
+        }
+      ]
+    });
+
+    var resources = {
+      'html5-boilerplate': {
+        name: 'HTML5 Boilerplate',
+        description: 'HTML5 Boilerplate is a professional front-end template' +
+          ' for building fast, robust, and adaptable web apps or sites.'
+      },
+      angular: {
+        name: 'Angular',
+        description: 'AngularJS is a toolset for building the framework most' +
+          ' suited to your application development.'
+      },
+      karma: {
+        name: 'Karma',
+        description: 'Spectacular Test Runner for JavaScript.'
+      }
+    };
+    $httpBackend.whenGET('/api/resource.php?id=html5-boilerplate').respond(resources['html5-boilerplate']);
+    $httpBackend.whenGET('/api/resource.php?id=angular').respond(resources.angular);
+    $httpBackend.whenGET('/api/resource.php?id=karma').respond(resources.karma);
+
+    $httpBackend.flush();
+
     expect(scope.awesomeThings.length).toBe(3);
-  });
+  }));
 });
