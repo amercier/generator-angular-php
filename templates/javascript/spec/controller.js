adc1dc235460e654c0ffa0db289040223a413d3a
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-06-21 17:12:02 +0200

    Fixed 'grunt-test' failing

--- templates/javascript/spec/controller.js
+++ templates/javascript/spec/controller.js
@@ -11,7 +11,7 @@ describe('Controller: <%= classedName %>Ctrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    $controller('<%= classedName %>Ctrl', {
       $scope: scope
     });
   }));
